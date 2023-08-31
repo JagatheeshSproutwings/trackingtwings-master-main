ab5ec58f647124a02dc93243cc22f61d8ba21a2f
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-08-31 13:08:22 +0530

    user create and edit

--- src/views/app-views/user_management/user/index.js
+++ src/views/app-views/user_management/user/index.js
@@ -23,8 +23,7 @@ import api from "configs/apiConfig";
 import Create from "./create";
 import Edit from "./edit";
 
-const { TextArea } = Input;
-const { Option } = Select;
+
 
 export const User = () => {
   const [list, setList] = useState(userData);
@@ -36,13 +35,7 @@ export const User = () => {
   const [isCardVisible, setIsCardVisible] = useState(false);
   const [isEditCardVisible, setIsEditCardVisible] = useState(false);
 
-  const [userList, setUserList] = useState([]);
-  const [selectedRoleId, setSelectedRoleId] = useState();
-  const [roleOptions, setRoleOptions] = useState([]);
-  const [selectedCountryId, setSelectedCountryId] = useState();
-  const [countryOptions, setCountryOptions] = useState([]);
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
+  
   const showDrawer = () => {
     setOpen(true);
   };
@@ -54,44 +47,7 @@ export const User = () => {
     setIsEditCardVisible(false);
     setIsTableVisible(false);
   };
-  const handleTableCard = () => {
-    setIsTableVisible(true);
-    setIsCardVisible(false);
-    setIsEditCardVisible(false);
-  };
-  const onFinish = async (values) => {
-    document.getElementById("name_er_span").textContent = "";
-    document.getElementById("email_er_span").textContent = "";
-    document.getElementById("mobile_er_span").textContent = "";
 
-    try {
-      await api.post("user/store", values);
-      window.location.reload(true);
-      setIsSubmitted(true);
-    } catch (error) {
-      if (error.response && error.response.status === 403) {
-        const errorData = error.response.data;
-        if (errorData.message && typeof errorData.message === "object") {
-          const validationErrors = errorData.message;
-          if (validationErrors.hasOwnProperty("name")) {
-            console.log(validationErrors.name);
-            document.getElementById("name_er_span").textContent =
-              validationErrors.name;
-          }
-          if (validationErrors.hasOwnProperty("email")) {
-            console.log(validationErrors.email);
-            document.getElementById("email_er_span").textContent =
-              validationErrors.email;
-          }
-          if (validationErrors.hasOwnProperty("mobile_no")) {
-            console.log(validationErrors.mobile_no);
-            document.getElementById("mobile_er_span").textContent =
-              validationErrors.mobile_no;
-          }
-        }
-      }
-    }
-  };
 
   const getRole = () => {
     return localStorage.getItem("role");
@@ -179,13 +135,8 @@ export const User = () => {
     loadUsers(setUserList);
   }, []);
 
-  const handleRoleIdChange = (roleID) => {
-    setSelectedRoleId(roleID);
-  };
 
-  const handleCountryIdChange = (countryId) => {
-    setSelectedCountryId(countryId);
-  };
+
 
   const tableColumns = [
     {
@@ -286,286 +237,13 @@ export const User = () => {
         </Col>
         <Col sm={24} md={10} lg={10}>
           {isCardVisible && (
-            <Row gutter={6}>
-              <Col>
-                <Card title="New User">
-                  <Flex>
-                    <div className="container">
-                      <Form
-                        size="small"
-                        name="registrationForm"
-                        onFinish={onFinish}
-                        layout="vertical"
-                      >
-                        <Row gutter={[8, 8]}>
-                          <Col sm={12} md={12} lg={12}>
-                            <Form.Item
-                              size="small"
-                              label="User Name"
-                              name="name"
-                              rules={[
-                                {
-                                  required: true,
-                                  message: "Please enter your name",
-                                },
-                              ]}
-                            >
-                              <Input />
-                            </Form.Item>
-                          </Col>
-                          <Col sm={12} md={12} lg={12}>
-                            <Form.Item
-                              size="small"
-                              label="E-Mail ID"
-                              name="email"
-                              rules={[
-                                {
-                                  required: true,
-                                  message: "Please enter your email",
-                                },
-                                {
-                                  type: "email",
-                                  message: "Please enter a valid email",
-                                },
-                              ]}
-                            >
-                              <Input />
-                            </Form.Item>
-                          </Col>
-                          <Col sm={12} md={12} lg={12}>
-                            {" "}
-                            <Form.Item
-                              size="small"
-                              label="Mobile No"
-                              name="mobile_no"
-                              rules={[
-                                {
-                                  required: true,
-                                  message: "Please enter your mobile no",
-                                },
-                                {
-                                  type: "text",
-                                  message: "Please enter a valid mobile no",
-                                },
-                              ]}
-                            >
-                              <Input />
-                            </Form.Item>
-                          </Col>
-                          <Col sm={12} md={12} lg={12}>
-                            <Form.Item
-                              size="small"
-                              label="Password"
-                              name="password"
-                              rules={[
-                                {
-                                  required: true,
-                                  message: "Please enter a password",
-                                },
-                                {
-                                  min: 6,
-                                  message:
-                                    "Password must be at least 6 characters long",
-                                },
-                              ]}
-                            >
-                              <Input.Password />
-                            </Form.Item>
-                          </Col>
-                          <Col sm={12} md={12} lg={12}>
-                            <Form.Item
-                              size="small"
-                              label="Confirm Password"
-                              name="c_password"
-                              rules={[
-                                {
-                                  required: true,
-                                  message: "Please confirm your password",
-                                },
-                                ({ getFieldValue }) => ({
-                                  validator(_, value) {
-                                    if (
-                                      !value ||
-                                      getFieldValue("password") === value
-                                    ) {
-                                      return Promise.resolve();
-                                    }
-                                    return Promise.reject(
-                                      new Error("Passwords do not match")
-                                    );
-                                  },
-                                }),
-                              ]}
-                            >
-                              <Input.Password />
-                            </Form.Item>
-                          </Col>
-                          <Col sm={12} md={12} lg={12}>
-                            <Form.Item
-                              size="small"
-                              label="Role"
-                              name="role_id"
-                              rules={[
-                                {
-                                  required: true,
-                                  message: "Please Select a Role",
-                                },
-                              ]}
-                            >
-                              <Select
-                                showSearch
-                                placeholder="Select Role"
-                                optionFilterProp="children"
-                                onChange={handleRoleIdChange}
-                                value={selectedRoleId}
-                                filterOption={(input, option) =>
-                                  option.children
-                                    .toLowerCase()
-                                    .indexOf(input.toLowerCase()) >= 0
-                                }
-                              >
-                                {Array.isArray(roleOptions) ? (
-                                  roleOptions.map((role) => (
-                                    <Option key={role.id} value={role.id}>
-                                      {role.name}
-                                    </Option>
-                                  ))
-                                ) : (
-                                  <Option value="Loading">Loading...</Option>
-                                )}
-                              </Select>
-                            </Form.Item>
-                          </Col>
-                          <Col sm={24} md={24} lg={24}>
-                            <Form.Item
-                              size="small"
-                              label="Address"
-                              name="address"
-                              rules={[
-                                {
-                                  required: true,
-                                  message: "Please enter a Address",
-                                },
-                              ]}
-                            >
-                              <TextArea
-                                rows={4}
-                                placeholder="Please Enter Adress"
-                                maxLength={100}
-                              />
-                            </Form.Item>
-                          </Col>
-                        </Row>
-                        <Row align={"middle"}>
-                          <Col sm={12} md={12} lg={12}>
-                            <Form.Item
-                              size="small"
-                              label="Country"
-                              name="country_id"
-                              rules={[
-                                {
-                                  required: true,
-                                  message: "Please Select a Country",
-                                },
-                              ]}
-                            >
-                              <Select
-                                showSearch
-                                placeholder="Select Country"
-                                optionFilterProp="children"
-                                onChange={handleCountryIdChange}
-                                value={selectedCountryId}
-                                filterOption={(input, option) =>
-                                  option.children
-                                    .toLowerCase()
-                                    .indexOf(input.toLowerCase()) >= 0
-                                }
-                              >
-                                {Array.isArray(countryOptions) ? (
-                                  countryOptions.map((country) => (
-                                    <Option key={country.id} value={country.id}>
-                                      {country.country_name}
-                                    </Option>
-                                  ))
-                                ) : (
-                                  <Option value="Loading">Loading...</Option>
-                                )}
-                              </Select>
-                            </Form.Item>
-                          </Col>
-                        </Row>
-
-                        <Row align={"middle"}>
-                          <Col span={12}>
-                            <Form.Item>
-                              <Space wrap>
-                                <Button
-                                  type="primary"
-                                  shape="round"
-                                  htmlType="submit"
-                                >
-                                  Register
-                                </Button>
-                                <Button
-                                  type="primary"
-                                  shape="round"
-                                  onClick={handleTableCard}
-                                >
-                                  Back
-                                </Button>
-                              </Space>
-                            </Form.Item>
-                          </Col>
-                        </Row>
-
-                        <span
-                          id="name_er_span"
-                          style={{
-                            color: "red",
-                            fontSize: "12px",
-                            fontWeight: "bold",
-                            fontFamily: "sans-serif",
-                          }}
-                        ></span>
-
-                        <span
-                          id="email_er_span"
-                          style={{
-                            color: "red",
-                            fontSize: "12px",
-                            fontWeight: "bold",
-                            fontFamily: "sans-serif",
-                          }}
-                        ></span>
-
-                        <span
-                          id="mobile_er_span"
-                          style={{
-                            color: "red",
-                            fontSize: "12px",
-                            fontWeight: "bold",
-                            fontFamily: "sans-serif",
-                          }}
-                        ></span>
-                      </Form>
-                      {isSubmitted && (
-                        <div style={{ marginTop: "16px" }}>
-                          <Alert
-                            message="Form submitted successfully!"
-                            type="success"
-                          />
-                        </div>
-                      )}
-                    </div>
-                  </Flex>
-                </Card>
-              </Col>
-            </Row>
+           
           )}
         </Col>
       </Row>
 
       <Create />
+      <Edit />
     </>
   );
 };
