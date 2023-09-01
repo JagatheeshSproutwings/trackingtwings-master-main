97a5421c004d09d02670dd664bc2e7d911264209
JagatheeshSproutwings <jagatheesan.p@sproutwings.in>
2023-09-01 10:39:17 +0530

    Report Page Commit

--- src/views/app-views/report_management/index.js
+++ src/views/app-views/report_management/index.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react'
-import { Table, Button, Row,Col,Card, Drawer, Select, Input, Form, Space,Checkbox,DatePicker } from "antd";
+import { Table, Button, Row,Col,Card, Drawer, Select, Input, Form, Space,Checkbox,DatePicker,notification } from "antd";
 import IdleReport from './idle_report'
 import ParkingReport from './parking_report';
 import PlaybackReport from './playback_report';
@@ -9,67 +9,104 @@ import Flex from "components/shared-components/Flex";
 import api from "configs/apiConfig";
 const { Option } = Select;
 const { RangePicker } = DatePicker;
+const NotificationType = 'success' | 'info' | 'warning' | 'error';
 const Report = () => {
     // Use State Values 
     const [isLoading, setIsLoading] = useState(false);
     const [user_role,SetUserRole] = useState("");
+    const [currentReport,SetReport] = useState("");
+    const [currentUser,SetCurrentUser] = useState("");
+    const [currentRole,SetCurrentRole] = useState("");
     const token = useSelector((state) => state.auth);
     
+    const handleReport = (report) => {
+        if(currentUser!='')
+        {
+            SetReport(report);
+        }
+      };
+
+      const user = () => {
+        return localStorage.getItem("id");
+      };
+      const role = () => {
+        return localStorage.getItem("role");
+      };
+      const admin = () => {
+        return localStorage.getItem("admin_id");
+      };
+      const distributor = () => {
+        return localStorage.getItem("distributor_id");
+      };
+      const dealer = () => {
+        return localStorage.getItem("dealer_id");
+      };
+      const subdealer = () => {
+        return localStorage.getItem("subdealer_id");
+      }; 
+       
     const newLocal = <Card>
         <Form layout="vertical" size='small'>
-            <Form.Item labelCol={{
-          span: 4,
-        }} label="Admin" size='small' rules={[
+            {currentRole==1 || currentRole==2  && (
+            <Form.Item  label="Admin" name="admin_id" size='small' rules={[
                 {
                     required: true,
                 },
             ]}>
                 <Select >
-                    <Select.Option value="demo">Demo</Select.Option>
+                    <Select.Option value="1">Acute</Select.Option>
                 </Select>
-            </Form.Item>
-            <Form.Item label="Distributor" rules={[
+            </Form.Item> )}
+            {currentRole==2 || currentRole==3  && (
+            <Form.Item label="Distributor" name="dealer_id" rules={[
                 {
                     required: true,
                 },
             ]}>
                 <Select >
-                    <Select.Option value="demo">Demo</Select.Option>
+                    <Select.Option value="1">Acute Distributor</Select.Option>
                 </Select>
-            </Form.Item>
-            <Form.Item label="Dealer" rules={[
+            </Form.Item>)}
+
+            {currentRole==3 || currentRole==4  && (
+            <Form.Item label="Dealer" name="dealer_id" rules={[
                 {
                     required: true,
                 },
             ]}>
-                <Select defaultValue="demo">
-                    <Select.Option value="demo">Demo</Select.Option>
+                <Select defaultValue="1">
+                    <Select.Option value="1">Acute Dealer</Select.Option>
                 </Select>
             </Form.Item>
-            <Form.Item label="SubDealer" rules={[
+            )}
+            {currentRole==3 || currentRole==4  && (
+            <Form.Item label="SubDealer" name="subdealer_id" rules={[
                 {
                     required: true,
                 },
             ]}>
-                <Select defaultValue="demo">
-                    <Select.Option value="demo">Demo</Select.Option>
+                <Select >
+                    <Select.Option value="1">Acute Sub Dealer</Select.Option>
                 </Select>
             </Form.Item>
-            <Form.Item label="Customer" rules={[
+            )}
+
+            <Form.Item label="Customer" defaultValue={currentUser} name="customer_id" rules={[
                 {
                     required: true,
                 },
             ]}>
-                <Select defaultValue="demo">
-                    <Select.Option value="demo">Demo</Select.Option>
+                <Select >
+                    <Select.Option value="10">{currentUser}</Select.Option>
                 </Select>
             </Form.Item>
+            
             <Form.Item label="Reports" rules={[
                 {
                     required: true,
                 },
-            ]}>
-                <Select>
+            ]} name="report_id">
+                <Select onChange={handleReport}>
                 <Select.Option value="1">Idle Report</Select.Option>
                 <Select.Option value="2">Parking Report</Select.Option>
                 <Select.Option value="3">Playback Report</Select.Option>
@@ -79,18 +116,41 @@ const Report = () => {
         </Form>
 
     </Card>;
+
+    useEffect(() => {
+        console.log(role());
+        
+        if(role()== 6 && user()!='')
+        {
+            console.log(user());
+            SetCurrentUser(user());
+        }
+        SetCurrentRole(role());
+    }, []);
+
   return (
+    
     <>
+    
     <Row gutter={6}>
         <Col xs={24} sm={24} xl={4} md={4} lg={4} xxl={4}>
             {newLocal}
         </Col>
         <Col xs={24} sm={24} xl={20} md={20} lg={20} xxl={20}>
-            
-            <IdleReport/>
-            <ParkingReport/>
-            <PlaybackReport/>
-            <KeyonKeyOffReport/>
+        {(() => {
+        switch (currentReport) {
+          case '1':
+            return <IdleReport/>;
+          case '2':
+            return <ParkingReport/>;
+          case '3':
+            return <PlaybackReport/>;
+            case '4':
+                return <KeyonKeyOffReport/>
+          default:
+            return "";
+        }
+      })()}
         </Col>
     </Row>
     </>
--- src/views/app-views/report_management/parking_report.js
+++ src/views/app-views/report_management/parking_report.js
@@ -1,11 +1,217 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Table, Button, Card, Select, DatePicker } from "antd";
+import Flex from "components/shared-components/Flex";
+import { FileExcelOutlined, SearchOutlined } from "@ant-design/icons";
+import api from "configs/apiConfig";
+import { Excel } from "antd-table-saveas-excel";
 
-const ParkingReport = () => {
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+
+export const ParkingReport = () => {
+  const [parkingList, setParkingList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedDateRange, setSelectedDateRange] = useState(null);
+  const [selectedVehicleId, setSelectedVehicleId] = useState("All");
+  const [vehicleOptions, setVehicleOptions] = useState([]);
+
+  useEffect(() => {
+    async function fetchVehicleOptions() {
+      try {
+        const response = await api.get("vehicle_list");
+        if (response.data.success) {
+          setVehicleOptions(response.data.data);
+        } else {
+          console.error("API request was not successful");
+        }
+      } catch (error) {
+        console.error("Error fetching vehicle options:", error);
+      }
+    }
+
+    fetchVehicleOptions();
+  }, []);
+
+  const handleDateRangeChange = (dateRange) => {
+    setSelectedDateRange(dateRange);
+  };
+
+  const handleVehicleIdChange = (vehicleId) => {
+    setSelectedVehicleId(vehicleId);
+  };
+
+  const handleSearchClick = async () => {
+    // Log the actual selected values
+    console.log("Selected Date Range:", selectedDateRange);
+    if (selectedDateRange) {
+      const [startDate, endDate] = selectedDateRange;
+      console.log("Start Date:", startDate.format("YYYY-MM-DD"));
+      console.log("End Date:", endDate.format("YYYY-MM-DD"));
+    }
+    console.log("Selected Vehicle ID:", selectedVehicleId);
+
+    const data = {
+      start_day: selectedDateRange
+        ? selectedDateRange[0].format("YYYY-MM-DD HH:mm:ss")
+        : null,
+      end_day: selectedDateRange
+        ? selectedDateRange[1].format("YYYY-MM-DD HH:mm:ss")
+        : null,
+      device_imei: selectedVehicleId === "0" ? null : selectedVehicleId,
+    };
+
+    setParkingList([]); // Clear previous data
+    setIsLoading(true);
+
+    try {
+      const parking_data = await api.post("get_parking_report", data);
+      console.log(parking_data.data);
+
+      if (parking_data.data && Array.isArray(parking_data.data.data)) {
+        const processedData = parking_data.data.data.map((item) => ({
+          s_no: item.id,
+          vehicle_name: item.vehicle_name,
+          start_date: item.start_datetime,
+          end_date: item.end_datetime,
+          location: item.start_latitude + ":" + item.start_longitude,
+          duration: item.parking_duration,
+        }));
+
+        console.log(processedData);
+
+        setParkingList(processedData);
+
+        console.log("parkingList:", parkingList);
+      } else {
+        setParkingList([]); // Clear the list when no data is found
+        console.log(
+          "Response data is not in the expected format:",
+          parking_data.data
+        );
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    setIsLoading(false);
+  };
+
+  const tableColumns = [
+    {
+      title: "S.No",
+      dataIndex: "s_no",
+    },
+    {
+      title: "Vehicle Name",
+      dataIndex: "vehicle_name",
+    },
+    {
+      title: "Start Date",
+      dataIndex: "start_date",
+    },
+    {
+      title: "End Date",
+      dataIndex: "end_date",
+    },
+    {
+      title: "Location",
+      dataIndex: "location",
+    },
+    {
+      title: "Duration",
+      dataIndex: "duration",
+    },
+    {
+      title: "Map View",
+      dataIndex: "map_view",
+    },
+  ];
+
+  const handleClick = () => {
+    const excel = new Excel();
+    excel
+      .addSheet("test")
+      .addColumns(tableColumns)
+      .addDataSource(parkingList, {
+        str2Percent: true,
+      })
+      .saveAs("Excel.xlsx");
+  };
+
   return (
-    <div>
-      <h3>Parking Report</h3>
-    </div>
-  )
-}
+    <>
+      <Card title="Parking Report">
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          mobileFlex={false}
+        >
+          <Flex className="mb-1" mobileFlex={false}>
+            <div className="mr-md-3 mr-3">
+              <RangePicker
+                showTime
+                name="range_picker"
+                format={"YYYY-MM-DD hh:mm:ss"}
+                onChange={handleDateRangeChange}
+              />
+            </div>
+            <div className="mr-md-3 mb-3">
+              <Select
+                defaultValue="All"
+                className="w-100"
+                style={{ minWidth: 180 }}
+                name="device_imei"
+                placeholder="Vehicle"
+                onChange={handleVehicleIdChange}
+                value={selectedVehicleId}
+              >
+                <Option value="All">All</Option>
+                {Array.isArray(vehicleOptions) ? (
+                  vehicleOptions.map((vehicle) => (
+                    <Option
+                      key={vehicle.device_imei}
+                      value={vehicle.device_imei}
+                    >
+                      {vehicle.vehicle_name}
+                    </Option>
+                  ))
+                ) : (
+                  <Option value="Loading">Loading...</Option>
+                )}
+              </Select>
+            </div>
+            <div className="mb-3 mr-3">
+              <Button
+                type="primary"
+                success
+                icon={<SearchOutlined />}
+                onClick={handleSearchClick}
+              >
+                Search
+              </Button>
+            </div>
+            <div className="mb-3">
+              <Button
+                type="primary"
+                icon={<FileExcelOutlined />}
+                onClick={handleClick}
+              >
+                Export
+              </Button>
+            </div>
+          </Flex>
+        </Flex>
+        <div className="table-responsive">
+          {isLoading ? (
+            <div>Loading...</div> // Display loading indicator
+          ) : parkingList.length > 0 ? (
+            <Table bordered columns={tableColumns} dataSource={parkingList} />
+          ) : (
+            <p>No Data Found</p>
+          )}
+        </div>
+      </Card>
+    </>
+  );
+};
 
-export default ParkingReport
+export default ParkingReport;
